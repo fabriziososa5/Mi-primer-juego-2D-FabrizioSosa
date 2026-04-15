ccc123fb448f09edf8f6b38881962faddc2b3fcb
fabriziososa5 <fabriziososa5.n@gmail.com>
2026-04-15 16:27:01 -0300

    Pantalla frontal

--- mi-primer-juego-2d-prototipado-digital-c-1-(fabrizio-sosa)-/main.gd
+++ mi-primer-juego-2d-prototipado-digital-c-1-(fabrizio-sosa)-/main.gd
@@ -12,11 +12,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -35,11 +38,12 @@ func _on_mob_timer_timeout():
 	
 	add_child(mob)
 
-
+	 
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
